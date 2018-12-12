d92922abac650d100ef1454ba276dd854f55d4b1
Your Name <you@example.com>
2018-12-12 11:31:20 +0100

    Serverless Angular with Firebase Course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -20,19 +20,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-      this.db.collection('courses').snapshotChanges()
-          .subscribe(snaps => {
 
-              const courses: Course[] = snaps.map(snap => {
-                  return <Course> {
-                      id: snap.payload.doc.id,
-                      ...snap.payload.doc.data()
-               ***REMOVED*****REMOVED***
-           ***REMOVED*****REMOVED***)
-
-              console.log(courses);
-
-       ***REMOVED*****REMOVED***);
 
 ***REMOVED***
 
