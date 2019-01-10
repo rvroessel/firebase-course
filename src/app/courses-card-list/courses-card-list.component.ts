20ed7c5c447073b1214939a0077df2cd7ede78df
JHades <jhades.dev@gmail.com>
2019-01-10 11:15:46 +0100

    Angular Firebase Course

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -31,13 +31,7 @@ export class CoursesCardListComponent implements OnInit {
             titles
      ***REMOVED*****REMOVED***;
 
-        const dialogRef = this.dialog.open(CourseDialogComponent,
-            dialogConfig);
-
-
-        dialogRef.afterClosed().subscribe(
-            val => console.log("Dialog output:", val)
-        );
+        this.dialog.open(CourseDialogComponent, dialogConfig);
 
  ***REMOVED*****REMOVED***
 
