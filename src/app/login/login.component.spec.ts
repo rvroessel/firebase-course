3b96dbd765f75cec5ece3b6aecbe20f7ea06b8f9
JHades <jhades.dev@gmail.com>
2019-01-16 11:34:44 +0100

    Angular Firebase Course

---
+++ src/app/login/login.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+ ***REMOVED*****REMOVED***)
+    .compileComponents();
+***REMOVED***));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+***REMOVED***);
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+***REMOVED***);
+});
