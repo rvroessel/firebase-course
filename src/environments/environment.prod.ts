c609d5bfed04feff554c375f6d6038ef39e25c80
Your Name <you@example.com>
2019-02-07 13:56:48 +0100

    Serverless Angular with Firebase Course

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 ***REMOVED***
-  production: true
+    production: true,
+ ***REMOVED*****REMOVED***
+        apiKey: "AIzaSyDg8hs704siPZ_chdtodWYpXicMHxz5nhQ",
+        authDomain: "fir-course-recording.firebaseapp.com",
+        databaseURL: "https://fir-course-recording.firebaseio.com",
+        projectId: "fir-course-recording",
+        storageBucket: "fir-course-recording.appspot.com",
+        messagingSenderId: "706691571098"
+ ***REMOVED*****REMOVED***
 ***REMOVED***
