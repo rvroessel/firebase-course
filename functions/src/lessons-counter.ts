9fc5e866602d8ba0cbbcf2bce869088d494edd4b
Your Name <you@example.com>
2019-02-20 15:26:06 +0100

    Serverless Angular with Firebase Course

---
+++ functions/src/lessons-counter.ts
@@ -0,0 +1,27 @@
+
+import***REMOVED*** as functions from 'firebase-functions';
+import {db} from './init';
+
+
+export const onAddLesson =
+functions.firestore.document('courses/{courseId}/lessons/{lessonId}')
+    .onCreate(async (snap,context) => {
+
+        console.log("Running onAddLesson trigger ...");
+
+        return db.runTransaction(async transaction => {
+
+            const courseRef = snap.ref.parent.parent;
+
+            const courseSnap = await transaction.get(courseRef);
+
+            const course = courseSnap.data();
+
+            const changes = {lessonsCount: course.lessonsCount + 1***REMOVED***
+
+            transaction.update(courseRef, changes);
+
+     ***REMOVED*****REMOVED***);
+
+ ***REMOVED*****REMOVED***);
+
