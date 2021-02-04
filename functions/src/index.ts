df95c57d7f80cf6f912fe8ce5d4b4ed9318ddefb
JHades <jhades.dev@gmail.com>
2021-02-04 15:09:19 +0100

    Firebase & Firestore In Depth course

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,9 @@
-import***REMOVED*** as functions from 'firebase-functions';
-import {db} from './init';
+import***REMOVED*** as functions from "firebase-functions";
 
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-import***REMOVED*** as express from "express";
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors({origin:true}));
-
-
-app.get('/courses', async (request, response) => {
-
-    const snaps = await db.collection('courses').get();
-
-    const courses:any[] = [];
-
-    snaps.forEach(snap => courses.push(snap.data()));
-
-    response.status(200).json({courses});
-
-});
-
-
-export const getCourses = functions.https.onRequest(app);
-
-export {onAddLesson, onDeleteLesson} from './lessons-counter';
-
-export {resizeThumbnail} from './image-upload';
+ export const helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+ });
