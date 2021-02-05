e4aaf16af2810901d2b4388e02adddfe0fdf4a05
JHades <jhades.dev@gmail.com>
2021-02-05 17:11:50 +0100

    Firebase & Firestore In Depth course

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,26 +17,25 @@ export class LoginComponent implements OnInit, OnDestroy {
     ui: firebaseui.auth.AuthUI;
 
     constructor(private afAuth: AngularFireAuth,
-                private router:Router,
-                private ngZone: NgZone) {
+                private router: Router) {
 
  ***REMOVED*****REMOVED***
 
     ngOnInit() {
-       this.afAuth.app.then((app) => {
-         const uiConfig = {
-           signInOptions: [
-             GoogleAuthProvider.PROVIDER_ID,
-             EmailAuthProvider.PROVIDER_ID
-           ],
-           callbacks: {
-             signInSuccessWithAuthResult: this
-               .onLoginSuccessful
-               .bind(this)
-        ***REMOVED*****REMOVED***
-      ***REMOVED*****REMOVED***;
-          this.ui = new firebaseui.auth.AuthUI(app.auth());
-          this.ui.start('#firebaseui-auth-container', uiConfig);
+        this.afAuth.app.then((app) => {
+            const uiConfig = {
+                signInOptions: [
+                    GoogleAuthProvider.PROVIDER_ID,
+                    EmailAuthProvider.PROVIDER_ID
+                ],
+                callbacks: {
+                    signInSuccessWithAuthResult: this
+                        .onLoginSuccessful
+                        .bind(this)
+             ***REMOVED*****REMOVED***
+         ***REMOVED*****REMOVED***;
+            this.ui = new firebaseui.auth.AuthUI(app.auth());
+            this.ui.start('#firebaseui-auth-container', uiConfig);
      ***REMOVED*****REMOVED***);
  ***REMOVED*****REMOVED***
 
@@ -46,9 +45,9 @@ export class LoginComponent implements OnInit, OnDestroy {
 
     onLoginSuccessful(result) {
 
-        console.log("Firebase UI result:", result);
+        console.log('Firebase UI result:', result);
 
-        this.ngZone.run(() => this.router.navigateByUrl('/courses'));
+        this.router.navigateByUrl('/courses');
 
  ***REMOVED*****REMOVED***
 
