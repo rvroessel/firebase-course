6a3cf6005c148c1f6fd1c27afc9e7490d231d938
JHades <jhades.dev@gmail.com>
2021-02-05 12:52:48 +0100

    Firebase & Firestore In Depth course

---
+++ try-emulator/src/app/model/course.ts
@@ -0,0 +1,14 @@
+
+
+export interface Course {
+    id:string;
+    titles: {
+        description:string;
+        longDescription: string;
+ ***REMOVED*****REMOVED***;
+    iconUrl: string;
+    uploadedImageUrl:string;
+    courseListIcon: string;
+    categories:string[];
+    lessonsCount:number;
+}
