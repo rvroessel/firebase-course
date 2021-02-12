3627297d1838e25d532bdc0e62f58c7bde040110
JHades <jhades.dev@gmail.com>
2021-02-12 15:00:12 +0100

    Firebase & Firestore In Depth course

---
+++ functions/src/auth/set-custom-claims.ts
@@ -0,0 +1,12 @@
+import***REMOVED*** as functions from 'firebase-functions';
+
+const admin = require('firebase-admin');
+
+export default async (request, response) => {
+
+  functions.logger.info("Setting custom claims");
+
+  await admin.auth().setCustomUserClaims(uid, { admin: true });
+
+  response.send("Hello from Firebase!");
+}
