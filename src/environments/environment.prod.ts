4fc8c6828c882289f52b6fb09bc354224cdeba43
JHades <jhades.dev@gmail.com>
2021-02-24 14:51:07 +0100

    Firebase & Firestore In Depth course

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 ***REMOVED***
-  production: true
+  production: true,
+***REMOVED***
+***REMOVED***
+    apiKey: "AIzaSyAsrcKEDOFUC9fKMxxng0Fi6PjqNVQj8vk",
+    authDomain: "fir-course-v2.firebaseapp.com",
+    projectId: "fir-course-v2",
+    storageBucket: "fir-course-v2.appspot.com",
+    messagingSenderId: "507380388733",
+    appId: "1:507380388733:web:1f20583d30ea71e02cb214"
+***REMOVED***
+***REMOVED***
+    createUser: "https://us-central1-fir-course-v2.cloudfunctions.net/createUser"
+***REMOVED***
 ***REMOVED***
