4dd572c6282320f17001aea43038e6d2b17cbfe1
Your Name <you@example.com>
2021-03-19 14:58:36 +0100

    Firebase & AngularFire In Depth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,6 +26,16 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
+        this.reloadCourses();
+
+ ***REMOVED*****REMOVED***
+
+    reloadCourses() {
+
+        this.beginnersCourses$ = this.coursesService.loadCoursesByCategory("BEGINNER");
+
+        this.advancedCourses$ = this.coursesService.loadCoursesByCategory("ADVANCED");
+
  ***REMOVED*****REMOVED***
 
 }
