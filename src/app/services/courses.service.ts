bb689761e96e124ae588c21bf734896f99e20e39
Your Name <you@example.com>
2021-04-15 10:14:41 +0200

    Firebase & AngularFire In Depth

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from "@angular/core";
 import {AngularFirestore} from "@angular/fire/firestore";
-import {Observable} from "rxjs";
+import {from, Observable, of} from "rxjs";
 import {Course} from "../model/course";
-import {map,tap} from "rxjs/operators";
+import {concatMap, map, tap} from "rxjs/operators";
 import {convertSnaps} from "./db-utils";
 
 
@@ -16,7 +16,43 @@ export class CoursesService {
  ***REMOVED*****REMOVED***
 
     createCourse(newCourse: Partial<Course>, courseId?:string) {
+        return this.db.collection("courses",
+                ref => ref.orderBy("seqNo", "desc").limit(1))
+            .get()
+            .pipe(
+                concatMap(result => {
 
+                    const courses = convertSnaps<Course>(result);
+
+                    const lastCourseSeqNo = courses[0]?.seqNo ?? 0;
+
+                    const course = {
+                        ...newCourse,
+                        seqNo: lastCourseSeqNo + 1
+                 ***REMOVED*****REMOVED***
+
+                    let save$: Observable<any>;
+
+                    if (courseId) {
+                        save$ = from(this.db.doc(`courses/${courseId}`).set(course));
+                 ***REMOVED*****REMOVED***
+                    else {
+                        save$ = from(this.db.collection("courses").add(course));
+                 ***REMOVED*****REMOVED***
+
+                    return save$
+                        .pipe(
+                            map(res => {
+                                return {
+                                    id: courseId ?? res.id,
+                                    ...course
+                             ***REMOVED*****REMOVED***
+                         ***REMOVED*****REMOVED***)
+                        );
+
+
+             ***REMOVED*****REMOVED***)
+            )
  ***REMOVED*****REMOVED***
 
     loadCoursesByCategory(category:string): Observable<Course[]> {
