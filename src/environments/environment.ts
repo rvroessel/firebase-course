0cb60c43699936505f76d20b488daf846d9c3bbe
JHades <jhades.dev@gmail.com>
2021-05-25 18:23:22 +0200

    Upgrade to Angular 12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -25,4 +25,4 @@
 ***REMOVED*** This import should be commented out in production mode because it will have a negative impact
 ***REMOVED*** on performance if an error is thrown.
 ***REMOVED***/
-import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+***REMOVED***
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,7 +57,7 @@
 ***REMOVED*****************************************************************************************************
 ***REMOVED*** Zone JS is required by default for Angular itself.
 ***REMOVED***/
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 ***REMOVED*****************************************************************************************************
