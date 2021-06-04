acf2a34736fb121b5c5dceab73ebf0581c9ac313
Your Name <you@example.com>
2021-06-04 15:03:21 +0200

    Firebase & AngularFire In Depth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,49 +5,70 @@ import {AboutComponent} from './about/about.component';
 import {CourseComponent} from './course/course.component';
 import {LoginComponent} from './login/login.component';
 import {CreateCourseComponent} from './create-course/create-course.component';
-import {AngularFireAuthGuard, hasCustomClaim, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+import {AngularFireAuthGuard, customClaims, hasCustomClaim, redirectUnauthorizedTo}
+    from '@angular/fire/auth-guard';
 import {CreateUserComponent} from './create-user/create-user.component';
 import {CourseResolver} from "./services/course.resolver";
+import {pipe} from "rxjs";
+import {map} from "rxjs/operators";
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
+const adminOnly = () => hasCustomClaim("admin");
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-***REMOVED***
-  {
-    path: 'create-course',
-    component: CreateCourseComponent
-
-***REMOVED***
-  {
-    path: 'create-user',
-    component: CreateUserComponent
-
-***REMOVED***
-  {
-    path: 'about',
-    component: AboutComponent
-***REMOVED***
-  {
-    path: 'login',
-    component: LoginComponent
-***REMOVED***
-  {
-    path: 'courses/:courseUrl',
-    component: CourseComponent,
-    resolve: {
-        course: CourseResolver
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [AngularFireAuthGuard],
+        data: {
+            authGuardPipe: redirectUnauthorizedToLogin
+     ***REMOVED*****REMOVED***
  ***REMOVED*****REMOVED***
-***REMOVED***
-  {
-    path: '**',
-    redirectTo: '/'
-***REMOVED***
+    {
+        path: 'create-course',
+        component: CreateCourseComponent,
+        canActivate: [AngularFireAuthGuard],
+        data: {
+            authGuardPipe: adminOnly
+     ***REMOVED*****REMOVED***
+ ***REMOVED*****REMOVED***
+    {
+        path: 'create-user',
+        component: CreateUserComponent,
+        canActivate: [AngularFireAuthGuard],
+        data: {
+            authGuardPipe: adminOnly
+     ***REMOVED*****REMOVED***
+ ***REMOVED*****REMOVED***
+    {
+        path: 'about',
+        component: AboutComponent
+ ***REMOVED*****REMOVED***
+    {
+        path: 'login',
+        component: LoginComponent
+ ***REMOVED*****REMOVED***
+    {
+        path: 'courses/:courseUrl',
+        component: CourseComponent,
+        resolve: {
+            course: CourseResolver
+     ***REMOVED*****REMOVED***
+        canActivate: [AngularFireAuthGuard],
+        data: {
+            authGuardPipe: redirectUnauthorizedToLogin
+     ***REMOVED*****REMOVED***
+ ***REMOVED*****REMOVED***
+    {
+        path: '**',
+        redirectTo: '/'
+ ***REMOVED*****REMOVED***
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
