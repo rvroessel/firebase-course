522410d30dde5d8597138ca5d165c15ac3e0ca6f
Your Name <you@example.com>
2021-06-25 14:25:30 +0200

    Firebase & AngularFire In Depth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@
       appId: "1:927953565493:web:0d4a8e79cc45fd38733e7c"
 ***REMOVED***
 ***REMOVED***
-
+      createUser: "http://localhost:5001/fir-course-recording-c7f3e/us-central1/createUser"
 ***REMOVED***
 ***REMOVED***
 
