dfd39535eb390db912a40828e0ffc9fd10fca333
Your Name <you@example.com>
2021-06-28 17:10:13 +0200

    Firebase & AngularFire In Depth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 ***REMOVED***
-  production: true
+    production: true,
+ ***REMOVED*****REMOVED***
+ ***REMOVED*****REMOVED***
+        apiKey: "AIzaSyB9LOREMGhj1jpVXOHTKIwQu2oM7pVfjQg",
+        authDomain: "fir-course-recording-c7f3e.firebaseapp.com",
+        projectId: "fir-course-recording-c7f3e",
+        storageBucket: "fir-course-recording-c7f3e.appspot.com",
+        messagingSenderId: "927953565493",
+        appId: "1:927953565493:web:0d4a8e79cc45fd38733e7c"
+ ***REMOVED*****REMOVED***
+ ***REMOVED*****REMOVED***
+        createUser: " https://us-central1-fir-course-recording-c7f3e.cloudfunctions.net/createUser"
+ ***REMOVED*****REMOVED***
 ***REMOVED***
